1509518949b5884ca953194f1fbeb9102a325699
waleed21121 <wa581509@gmail.com>
2025-10-23 12:46:41 +0300

    Created category schema

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './schemas/category.schema';
 
 @Module({
   controllers: [CategoryController],
   providers: [CategoryService],
+  imports: [
+    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }])
+  ],
 })
 export class CategoryModule {}
