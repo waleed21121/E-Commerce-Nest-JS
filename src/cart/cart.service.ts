cd8dcf908f7280b8acf2cb3030d2008439f3ee22
waleed21121 <wa581509@gmail.com>
2025-10-30 11:09:53 +0300

    Refactored cart services

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -12,6 +12,8 @@ import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
 import { PaginationQueryDto } from 'src/common/pagination/dtos/pagination-query.dto';
 import { PaginationService } from 'src/common/pagination/pagination.service';
+import { ProductDocument } from 'src/product/schemas/product.schema ';
+import { TaxDocument } from 'src/tax/schemas/tax-schema';
 
 @Injectable()
 export class CartService {
@@ -31,74 +33,7 @@ export class CartService {
     // will throw an error if there is no product
     const product = (await this.productService.findOne(createCartDto.item.product)).date;
 
-    if(createCartDto.item.color) {
-      if(!product.color.includes(createCartDto.item.color)) {
-        throw new BadRequestException(`there is no ${createCartDto.item.color} of product with id: ${product._id}.`)
-      }
-    }
-
-    if(product.quantity < createCartDto.item.quantity) {
-      throw new BadGatewayException(`the quantity you is not avilable at the moment, you can buy at most ${product.quantity}.`)
-    }
-
-    // will throw an error if there is no tax;
-    const tax = await this.taxService.findOne();
-
-    createCartDto.item.priceAtAddTime = product.price * createCartDto.item.quantity;
-    createCartDto.item.discountAtAddTime = (
-      product.discountPrice ? (product.price - product.discountPrice) * createCartDto.item.quantity : 0
-    );
-
-    // total quantity price
-    createCartDto.item.totalItemPrice = (
-      createCartDto.item.priceAtAddTime -
-      createCartDto.item.discountAtAddTime
-    );
-    createCartDto.totalPrice = createCartDto.item.totalItemPrice;
-
-    // tax computation
-    createCartDto.tax = (tax.shippingTaxRate + tax.productTaxRate) * createCartDto.item.totalItemPrice;
-
-    let cart = await this.cartModel.findOne({ user: createCartDto.user });
-    if (!cart) {
-      const newCart = await this.cartModel.create({
-        user: createCartDto.user,
-        items: [createCartDto.item],
-        tax: createCartDto.tax,
-        totalPrice: createCartDto.totalPrice
-      });
-
-      return {
-        status: 201,
-        message: `Product added successfully to your cart`,
-        data: newCart
-      }
-    }
-    // adding tax & total price
-    cart.tax += createCartDto.tax;
-    cart.totalPrice += createCartDto.totalPrice;
-
-    let productFound: boolean = false;
-    cart.items.forEach((item) => {
-      // console.log(item.product, createCartDto.item.product);
-      
-      if (item.product.toString() === createCartDto.item.product && item.color === createCartDto.item.color) {
-        productFound = true;
-        item.quantity += createCartDto.item.quantity;
-        item.priceAtAddTime += createCartDto.item.priceAtAddTime;
-        item.discountAtAddTime += createCartDto.item.discountAtAddTime;
-        item.totalItemPrice += createCartDto.item.totalItemPrice;
-      }
-    });
-
-    if (!productFound) {
-      if(cart.items.length === 5) {
-        throw new BadRequestException(`your cart is full.`)
-      }
-      cart.items.push(createCartDto.item);
-    }
-
-    await cart.save();
+    const cart = await this.addToCart(user._id.toString(), product, createCartDto.item.quantity, createCartDto.item.color);
 
     return {
       status: 201,
@@ -109,7 +44,7 @@ export class CartService {
 
   async findAll(paginationDto: PaginationQueryDto) {
     const carts = await this.paginationService.paginateQuery<CartDocument, Cart>(paginationDto, this.cartModel);
-    if(carts.data.length === 0) {
+    if (carts.data.length === 0) {
       throw new NotFoundException();
     }
 
@@ -118,12 +53,12 @@ export class CartService {
 
   async findOne(id: string, userId: string) {
     const cart = await this.cartModel.findById(id);
-    if(!cart) {
+    if (!cart) {
       throw new NotFoundException();
     }
 
     const role: string = this.request['user']['role'];
-    if(role === 'user' && userId !== cart.user.toString()) {
+    if (role === 'user' && userId !== cart.user.toString()) {
       throw new UnauthorizedException(`you are not allowed to view this cart`);
     }
 
@@ -138,80 +73,152 @@ export class CartService {
     // will throw an error if user is not found
     const user = await this.userService.findOneById(updateCartDto.user);
 
-    const cart = await this.cartModel.findOne({user: user._id});
-    if(!cart) {
-      throw new BadRequestException(`user with id: ${updateCartDto.user} doesn't have a cart`);
+    const { cart, productId } = await this.removeFromCart(updateCartDto.user, updateCartDto.item.item);
+
+    const product = (await this.productService.findOne(productId)).date;
+
+    const updatedCart = await this.addToCart(updateCartDto.user, product, updateCartDto.item.quantity, updateCartDto.item.color);
+
+    return {
+      status: 200,
+      message: 'cart updated successfully',
+      data: updatedCart
     }
-
-    const index = cart.items.findIndex((item) => {
-      if(item._id!.toString() === updateCartDto.item.item) {
-        return true;
-      }
-    })
-
-    if(index === -1) {
-      throw new BadRequestException(`you don't have this product in your cart`)
-    }
-
-    const productId = cart.items[index].product.toString();
-
-    // console.log(productId);
-    
-    await this.remove({userId: updateCartDto.user, itemId: updateCartDto.item.item});
-    return await this.create({
-      item: {
-        product: productId,
-        quantity: updateCartDto.item.quantity,
-        color: updateCartDto.item.color,
-        priceAtAddTime: 0,
-        discountAtAddTime: 0,
-        totalItemPrice: 0
-      },
-      user: updateCartDto.user,
-      tax: 0,
-      totalPrice: 0
-    })
   }
 
   async remove(deleteProductFromCartDto: DeleteFromCartDto) {
     // will throw an error if user is not found
     const user = await this.userService.findOneById(deleteProductFromCartDto.userId);
+    
+    await this.removeFromCart(deleteProductFromCartDto.userId, deleteProductFromCartDto.itemId);
 
-    // will throw an error if there is no product
-    // const product = (await this.productService.findOne(deleteProductFromCartDto.itemId)).date;
+    return {
+      status: 200,
+      message: 'product deleted successfully'
+    }
+  }
 
-    const cart = await this.cartModel.findOne({user: user._id});
-    if(!cart) {
-      throw new BadRequestException(`user with id: ${deleteProductFromCartDto.userId} doesn't have a cart`);
+  async addToCart(userId: string, product: ProductDocument, quantity: number, color?: string) {
+
+    if (color) {
+      if (!product.color.includes(color)) {
+        throw new BadRequestException(`there is no ${color} of product with id: ${product._id}.`)
+      }
     }
 
-    const index = cart.items.findIndex((item) => {
-      if(item._id!.toString() === deleteProductFromCartDto.itemId) {
-        return true;
-      }
-    })
-    
-    if(index === -1) {
-      throw new BadRequestException(`you don't have this item in your cart`)
+    if (product.quantity < quantity) {
+      throw new BadGatewayException(`the quantity you is not avilable at the moment, you can buy at most ${product.quantity}.`)
     }
 
     // will throw an error if there is no tax;
     const tax = await this.taxService.findOne();
 
-    const item = cart.items[index];
+    // get prices
+    const prices = this.computePrices(product, quantity, tax);
 
-    cart.totalPrice -= item.totalItemPrice;
-    cart.tax -= (tax.shippingTaxRate + tax.productTaxRate) * item.totalItemPrice 
+    const createCartDto = {
+      user: userId,
+      item: {
+        product: product._id.toString(),
+        quantity: quantity,
+        color: color,
+        priceAtAddTime: prices.priceAtAddTime,
+        discountAtAddTime: prices.discountAtAddTime,
+        totalItemPrice: prices.totalItemPrice
+      },
+      tax: prices.taxes,
+      totalPrice: prices.totalItemPrice
+    }
+    
+    let cart = await this.cartModel.findOne({ user: userId });
+    if (!cart) {
+      const newCart = await this.cartModel.create({
+        user: createCartDto.user,
+        items: [createCartDto.item],
+        tax: createCartDto.tax,
+        totalPrice: createCartDto.totalPrice
+      });
+
+      return newCart;
+    }
+
+    // adding tax & total price
+    cart.tax += createCartDto.tax;
+    cart.totalPrice += createCartDto.totalPrice;
+
+    let productFound: boolean = false;
+    cart.items.forEach((item) => {
+      if (item.product.toString() === createCartDto.item.product && item.color === createCartDto.item.color) {
+        productFound = true;
+        item.quantity += createCartDto.item.quantity;
+        item.priceAtAddTime += createCartDto.item.priceAtAddTime;
+        item.discountAtAddTime += createCartDto.item.discountAtAddTime;
+        item.totalItemPrice += createCartDto.item.totalItemPrice;
+      }
+    });
+
+    if (!productFound) {
+      if (cart.items.length === 5) {
+        throw new BadRequestException(`your cart is full.`)
+      }
+      cart.items.push(createCartDto.item);
+    }
+
+    await cart.save();
+
+    return cart
+  }
+
+  computePrices(product: ProductDocument, quantity: number, tax: TaxDocument) {
+    const priceAtAddTime = product.price * quantity;
+    const discountAtAddTime = (
+      product.discountPrice ? (product.price - product.discountPrice) * quantity : 0
+    );
+
+    // total quantity price
+    const totalItemPrice = priceAtAddTime - discountAtAddTime
+
+    // tax computation
+    const taxes = (tax.shippingTaxRate + tax.productTaxRate) * totalItemPrice;
+
+    return {
+      priceAtAddTime,
+      discountAtAddTime,
+      totalItemPrice,
+      taxes
+    }
+  }
+
+  async removeFromCart(userId: string, itemId: string) {
+    const cart = await this.cartModel.findOne({ user: userId });
+    if (!cart) {
+      throw new BadRequestException(`user with id: ${userId} doesn't have a cart`);
+    }
+
+    const index = cart.items.findIndex((item) => {
+      if (item._id!.toString() === itemId) {
+        return true;
+      }
+    })
+
+    if (index === -1) {
+      throw new BadRequestException(`you don't have this item in your cart`)
+    }
+
+    const tax = await this.taxService.findOne();
+
+    const item = cart.items[index];
 
     cart.items.splice(index, 1);
 
-    
-    
+    cart.totalPrice -= item.totalItemPrice;
+    cart.tax -= (tax.shippingTaxRate + tax.productTaxRate) * item.totalItemPrice
+
     await cart.save();
 
     return {
-      status: 200,
-      message: 'product deleted successfully'
+      cart,
+      productId: item.product,
     }
   }
 }
