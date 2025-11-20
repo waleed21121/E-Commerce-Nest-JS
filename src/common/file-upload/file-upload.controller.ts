6a3ba3363e0120f627d089ce1d1097cf35ce8487
waleed21121 <wa581509@gmail.com>
2025-11-20 10:32:55 +0200

    Added validation on image

--- src/common/file-upload/file-upload.controller.ts
+++ src/common/file-upload/file-upload.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('files')
 export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
 
+  @Roles(['admin', 'user'])
+  @UseGuards(AuthGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadImage(@UploadedFile() file: Express.Multer.File) {
+  uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024,
+            message: 'image must be at most 1MB'
+          }),
+          new FileTypeValidator({
+            fileType: /\/(jpg|jpeg|png)$/
+          })
+        ]
+      })
+    ) file: Express.Multer.File
+  ) {
     return this.fileUploadService.uploadFileToCloudinary(file);
   }
 
